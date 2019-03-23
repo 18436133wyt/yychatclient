ab2d458363194a21feb6cac50b4738137bf24a5a
18436133wyt <2974314547@qq.com>
2019-03-23 10:13:35 +0800

    第一次更新

---
+++ ppchatclient/src/com/ppchatclient/view/ClientLogin.java
@@ -0,0 +1,10 @@
+package com.ppchatclient.view;
+
+public class ClientLogin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ ppchatclient/src/com/ppchatclient/view/friendChat.java
@@ -0,0 +1,10 @@
+package com.ppchatclient.view;
+
+public class friendChat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
